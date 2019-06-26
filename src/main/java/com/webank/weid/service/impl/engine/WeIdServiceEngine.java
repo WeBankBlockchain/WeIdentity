d77cf6ed07148fab270e6ab60477006a33b7710d
chenhaozx <chenhaozx@live.com>
2019-06-26 20:29:16 +0800

    * optimize weid engine code

--- src/main/java/com/webank/weid/service/impl/engine/WeIdServiceEngine.java
+++ src/main/java/com/webank/weid/service/impl/engine/WeIdServiceEngine.java
@@ -23,19 +23,46 @@ import com.webank.weid.protocol.base.WeIdDocument;
 import com.webank.weid.protocol.response.ResponseData;
 
 /**
+ * this service engine calls the contract methods and process blockchain response data
+ * on FISCO BCOS 1.3.x or on FISCO BCOS 2.0 
  * @author tonychen 2019年6月21日
  */
 public interface WeIdServiceEngine {
 
-    ResponseData<Boolean> createWeId(String weId, String publicKey, String privateKey);
+	/**
+	 * 
+	 * @param weAddress identity on blockchain
+	 * @param publicKey public key of the identity
+	 * @param privateKey privateKey identity's private key
+	 * @return result
+	 */
+    ResponseData<Boolean> createWeId(String weAddress, String publicKey, String privateKey);
 
+    /**
+     * write attribute to blockchain
+     * @param weAddress identity on blockchain
+     * @param attributeKey the key of the attribute
+     * @param value the value of the attribute
+     * @param privateKey identity's private key
+     * @return result
+     */
     ResponseData<Boolean> setAttribute(
         String weAddress,
         String attributeKey,
         String value,
         String privateKey);
 
+    /**
+     * check if the weid exists on blockchain
+     * @param weId the weid of the entity
+     * @return result
+     */
     ResponseData<Boolean> isWeIdExist(String weId);
 
+    /**
+     * get weid document from blockchain
+     * @param weId the entity's weid
+     * @return weid document
+     */
     ResponseData<WeIdDocument> getWeIdDocument(String weId);
 }
