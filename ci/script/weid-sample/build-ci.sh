53f2df233924f55d9cf9a7e86f1e2c734c19f9ce
chenhaozx <chenhaozx@live.com>
2019-06-28 19:02:57 +0800

    * Enhance the CI pipeline (http-service and sample part)

--- ci/script/weid-sample/build-ci.sh
+++ ci/script/weid-sample/build-ci.sh
@@ -2,9 +2,15 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ];then
     echo "This is a master branch PR, starting Sample CI pipeline.."
+    chmod +x ci/script/build-ci.sh
+    ci/script/build-ci.sh
+
     # clone repo
     rm -rf weid-sample/
     git clone https://github.com/WeBankFinTech/weid-sample.git
+    cd weid-sample/
+    git checkout develop
+    cd ..
 
     # construct SDK jar version and file name
     cat build.gradle | grep "version =" > temp.ver
@@ -19,11 +25,20 @@ if [ "$TRAVIS_BRANCH" = "master" ];then
 
     # copy SDK jar to repo dependencies path and rename
     # requires repo to allow local dep first
-    # cp dist/app/$FILENAME weid-sample/dependencies/weid-java-sdk-pipeline.jar
+    mkdir -p weid-sample/dependencies
+    cp dist/app/$FILENAME weid-sample/dependencies/weid-java-sdk-pipeline.jar
+    mkdir -p weid-sample/libs
+    cp dist/lib/* weid-sample/libs
+
+    # copy config files
+    cp ecdsa_key weid-sample/keys/priv/
+    cp src/main/resources/* weid-sample/src/main/resources/
 
     # run repo ci scripts
     cd weid-sample/
-    # gradle build -x test
+    chmod +x *.sh
+    ./build.sh
+    ./sample-ci.sh
 else
     echo "This is not a master branch PR (commit omitted). CI skipped."
 fi
