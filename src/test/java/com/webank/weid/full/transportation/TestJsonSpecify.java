3349b74eafb34b0203ba5f790e665611535f5dfe
yg3630536 <125480926@qq.com>
2019-08-16 14:41:59 +0800

    fixed checkStyle

--- src/test/java/com/webank/weid/full/transportation/TestJsonSpecify.java
+++ src/test/java/com/webank/weid/full/transportation/TestJsonSpecify.java
@@ -3,17 +3,15 @@ package com.webank.weid.full.transportation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import com.webank.weid.constant.ErrorCode;
 import com.webank.weid.full.TestBaseServcie;
 import com.webank.weid.suite.api.transportation.TransportationFactory;
 import com.webank.weid.suite.api.transportation.inf.JsonTransportation;
 
 public class TestJsonSpecify extends TestBaseServcie {
-
-    private static final Logger logger = LoggerFactory.getLogger(TestJsonSpecify.class);
 
     /**
      * case: weid exist.
@@ -26,5 +24,7 @@ public class TestJsonSpecify extends TestBaseServcie {
         weIdList.add(createWeIdNew.getWeId());
         jsonTransportation = jsonTransportation.specify(weIdList);
         System.out.println(jsonTransportation);
+        Assert.assertNotNull(jsonTransportation);
+        
     }
 }
