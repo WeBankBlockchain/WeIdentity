1914aa8e0d238e8fb38e7c9fc8e2fb6a73f81bd0
chenhaozx <chenhaozx@live.com>
2019-12-13 11:22:17 +0800

    * support zkp credential

---
+++ src/main/java/com/webank/weid/protocol/cpt/Cpt111.java
@@ -0,0 +1,40 @@
+/*
+ *       Copyright© (2019) WeBank Co., Ltd.
+ *
+ *       This file is part of weid-java-sdk.
+ *
+ *       weid-java-sdk is free software: you can redistribute it and/or modify
+ *       it under the terms of the GNU Lesser General Public License as published by
+ *       the Free Software Foundation, either version 3 of the License, or
+ *       (at your option) any later version.
+ *
+ *       weid-java-sdk is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *       GNU Lesser General Public License for more details.
+ *
+ *       You should have received a copy of the GNU Lesser General Public License
+ *       along with weid-java-sdk.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.webank.weid.protocol.cpt;
+
+import com.github.reinert.jjschema.Attributes;
+
+import lombok.Data;
+
+/**
+ * User request issuer to sign credential.
+ * @author tonychen
+ */
+@Data
+@Attributes(title = "test CPT", description = "Reserved CPT 111")
+public class Cpt111 {
+
+	@Attributes(required = true, description = "CPT ID")
+    private String cptId;
+    @Attributes(required = true, description = "credential Signature Request", minimum = 1)
+    private String credentialSignatureRequest;
+    @Attributes(required = true, description = "User Nonce")
+    private String userNonce;
+}
