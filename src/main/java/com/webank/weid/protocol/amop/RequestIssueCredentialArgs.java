83fe129e29efa0300d77b8f4a559cb5b1e8cf06e
chenhaozx <chenhaozx@live.com>
2019-12-20 10:16:53 +0800

    * add parameters check for AMOP interface

--- src/main/java/com/webank/weid/protocol/amop/RequestIssueCredentialArgs.java
+++ src/main/java/com/webank/weid/protocol/amop/RequestIssueCredentialArgs.java
@@ -19,27 +19,34 @@
 
 package com.webank.weid.protocol.amop;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
 import com.webank.weid.protocol.amop.base.AmopBaseMsgArgs;
+import com.webank.weid.protocol.base.CredentialPojo;
 import com.webank.weid.protocol.base.PolicyAndPreCredential;
 import com.webank.weid.protocol.base.WeIdAuthentication;
 
 /**
  * args for RequestIssueCredential.
+ *
  * @author tonychen 2019年12月4日
  */
 @Getter
 @Setter
 public class RequestIssueCredentialArgs extends AmopBaseMsgArgs {
 
-    //private PresentationE presentation;
-
     /**
      * policyAndPreCredential from issuer.
      */
     private PolicyAndPreCredential policyAndPreCredential;
+
+    /**
+     * user's credential list.
+     */
+    private List<CredentialPojo> credentialList;
 
     /**
      * policy id.
