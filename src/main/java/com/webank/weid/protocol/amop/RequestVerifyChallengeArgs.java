6e23e93be7978a33084451f3a3363c36203ff603
chenhaozx <chenhaozx@live.com>
2020-03-14 16:20:28 +0800

    * modify checkStyle issue

--- src/main/java/com/webank/weid/protocol/amop/RequestVerifyChallengeArgs.java
+++ src/main/java/com/webank/weid/protocol/amop/RequestVerifyChallengeArgs.java
@@ -1,21 +1,21 @@
 package com.webank.weid.protocol.amop;
 
-import com.webank.weid.protocol.amop.base.AmopBaseMsgArgs;
-import com.webank.weid.protocol.base.Challenge;
-
 import lombok.Getter;
 import lombok.Setter;
 
+import com.webank.weid.protocol.amop.base.AmopBaseMsgArgs;
+import com.webank.weid.protocol.base.Challenge;
+
 /**
+ * args for verify challenge.
  * @author tonychen 2020年3月12日
- *
  */
 @Getter
 @Setter
-public class RequestVerifyChallengeArgs extends AmopBaseMsgArgs{
+public class RequestVerifyChallengeArgs extends AmopBaseMsgArgs {
 
-	private String channelId;
-	private String selfWeId;
-	private Challenge challenge;
-	private String signData;
+    private String channelId;
+    private String selfWeId;
+    private Challenge challenge;
+    private String signData;
 }
