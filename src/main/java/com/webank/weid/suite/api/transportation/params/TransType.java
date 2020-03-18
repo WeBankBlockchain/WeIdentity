1c7bcec6c3b697077743ae7e56215f074aef2926
yg36305356 <125480926@qq.com>
2020-03-18 23:00:02 +0800

    * Optimize code

--- src/main/java/com/webank/weid/suite/api/transportation/params/TransType.java
+++ src/main/java/com/webank/weid/suite/api/transportation/params/TransType.java
@@ -23,31 +23,31 @@ import com.webank.weid.constant.ErrorCode;
 import com.webank.weid.exception.WeIdBaseException;
 
 /**
- * 传输协议类型定义.
+ * 传输协议类型定义, 决定数据是走那种传输模式.
  * 
  * @author yanggang
  *
  */
-public enum TransmissionType {
+public enum TransType {
     /**
      * 通过AMOP请求数据.
      */
     AMOP(0),
     
     /**
-     * 通过HTTP请求数据.
-     */
-    HTTP(1),
-    
-    /**
      * 通过HTTPS请求数据.
      */
-    HTTPS(2)
+    HTTPS(1),
+    
+    /**
+     * 通过HTTP请求数据.
+     */
+    HTTP(2)
     ;
 
     private Integer code;
 
-    TransmissionType(Integer code) {
+    TransType(Integer code) {
         this.code = code;
     }
 
@@ -61,8 +61,8 @@ public enum TransmissionType {
      * @param code the Transmission type code
      * @return TransmissionType
      */
-    public static TransmissionType getTransmissionByCode(Integer code) {
-        for (TransmissionType type : TransmissionType.values()) {
+    public static TransType getTransmissionByCode(Integer code) {
+        for (TransType type : TransType.values()) {
             if (type.getCode() == code) {
                 return type;
             }
