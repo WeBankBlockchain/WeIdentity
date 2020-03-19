75363a0e6ec4d85371f8ef2f7415431823caa563
yg3630536 <125480926@qq.com>
2020-03-19 11:35:32 +0800

    * optimization code for comment

--- src/test/java/com/webank/weid/full/transportation/TestJsonSpecify.java
+++ src/test/java/com/webank/weid/full/transportation/TestJsonSpecify.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.webank.weid.exception.WeIdBaseException;
 import com.webank.weid.full.TestBaseService;
 import com.webank.weid.suite.api.transportation.TransportationFactory;
 import com.webank.weid.suite.api.transportation.inf.Transportation;
@@ -24,5 +25,22 @@ public class TestJsonSpecify extends TestBaseService {
         transportation = transportation.specify(weIdList);
         Assert.assertNotNull(transportation);
         
+    }
+    
+    /**
+     * case: specify again.
+     */
+    @Test
+    public void testJsonSpecify_again() {
+        Transportation transportation = TransportationFactory.newJsonTransportation();
+        List<String> weIdList = new ArrayList<>();
+        weIdList.add(createWeIdResult.getWeId());
+        weIdList.add(createWeIdNew.getWeId());
+        try {
+            transportation = transportation.specify(weIdList).specify(weIdList);
+            Assert.assertTrue(false);
+        } catch (WeIdBaseException e) {
+            Assert.assertTrue(true);
+        }
     }
 }
