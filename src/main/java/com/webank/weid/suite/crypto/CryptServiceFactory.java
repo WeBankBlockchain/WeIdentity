d2325bf057eab033391628daa5ac3ba923245742
yg36305356 <125480926@qq.com>
2020-04-24 10:38:01 +0800

    * add code for rsa

--- src/main/java/com/webank/weid/suite/crypto/CryptServiceFactory.java
+++ src/main/java/com/webank/weid/suite/crypto/CryptServiceFactory.java
@@ -40,6 +40,7 @@ public class CryptServiceFactory {
     
     static {
         cryptServiceMap.put(CryptType.AES.name(), new AesCryptService());
+        cryptServiceMap.put(CryptType.RSA.name(), new RsaCryptService());
     }
 
     /**
