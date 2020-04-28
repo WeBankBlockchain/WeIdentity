1b0666a4895eb5b7c5ddb3078af2047c8df42f0c
yg36305356 <125480926@qq.com>
2020-04-28 12:03:17 +0800

    * add code for ecies

--- src/main/java/com/webank/weid/suite/crypto/CryptService.java
+++ src/main/java/com/webank/weid/suite/crypto/CryptService.java
@@ -31,16 +31,16 @@ public interface CryptService {
     /**
      * 加密方法.
      * @param content 待加密字符串
-     * @param password 秘钥
+     * @param key 秘钥
      * @return 返回加密后的字符串数据
      */
-    public String encrypt(String content, String password) throws EncodeSuiteException;
+    public String encrypt(String content, String key) throws EncodeSuiteException;
     
     /**
      * 解密方法.
      * @param content 待解密字符串
-     * @param password 秘钥
+     * @param key 秘钥
      * @return 返回解密后的字符串数据
      */
-    public String decrypt(String content, String password) throws EncodeSuiteException;
+    public String decrypt(String content, String key) throws EncodeSuiteException;
 }
