f8bc5496994a823a217e866d4c2fbff130f4255f
yg36305356 <125480926@qq.com>
2020-06-05 16:25:41 +0800

    * support multi group operation

--- src/main/java/com/webank/weid/exception/InitWeb3jException.java
+++ src/main/java/com/webank/weid/exception/InitWeb3jException.java
@@ -32,11 +32,15 @@ public class InitWeb3jException extends WeIdBaseException {
     public InitWeb3jException(Throwable cause) {
         super(ErrorCode.LOAD_WEB3J_FAILED.getCodeDesc(), cause);
     }
-
+    
+    public InitWeb3jException(String message) {
+        super(message);
+    }
+    
     public InitWeb3jException() {
         super(ErrorCode.LOAD_WEB3J_FAILED.getCodeDesc());
     }
-
+    
     @Override
     public ErrorCode getErrorCode() {
         return ErrorCode.LOAD_WEB3J_FAILED;
