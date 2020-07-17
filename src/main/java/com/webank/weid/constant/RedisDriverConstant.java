26f1a1e874358b0645a4ab36ba75b3b3dfe5952b
karenlrx <karen_lrx@163.com>
2020-07-17 09:54:16 +0800

    add the redis persistence.

---
+++ src/main/java/com/webank/weid/constant/RedisDriverConstant.java
@@ -0,0 +1,48 @@
+package com.webank.weid.constant;
+
+/**
+ * @author karenli
+ * @program: weid-java-sdk
+ * @description: constant for Redisson
+ * @date 2020-07-03 16:40:56
+ */
+
+public class RedisDriverConstant {
+
+    /**
+     * redis execute status.
+     */
+    public static final Integer REDISSON_EXECUTE_FAILED_STATUS = 0;
+    public static final Integer REDISSON_EXECUTE_SUCESS_STATUS = 1;
+
+    public static final String DOMAIN_DEFAULT_INFO = "domain.defaultInfo";
+
+    public static final String REDISSON_URL_SINGLE="redisson.url.single";
+    public static final String REDISSON_URL_CLUSTER="redisson.url.cluster";
+
+    /**
+     * the default value for redis cluster config.
+     */
+
+    public static final Integer IDLE_CONNECTION_TIMEOUT = 10000;
+    public static final Integer PING_TIMEOUT=1000;
+    public static final Integer CONNECT_TIMEOUT=10000;
+    public static final Integer TIMEOUT=3000;
+    public static final Integer RETRY_ATTEMPTS=3;
+    public static final Integer RETRY_INTERVAL=1500;
+    public static final Integer RECONNECTION_TIMEOUT=3000;
+    public static final Integer FAILED_ATTEMPTS=3;
+    public static final String PASSWORD="DO_NOT_USE_IF_IT_IS_NOT_SET";
+    public static final Integer SUBSCRIPTIONS_PER_CONNECTION=5;
+    public static final String CLIENT_NAME=null;
+    public static final String LOAD_BALANCER_REF="MYLOADBALANCER";
+    public static final Integer SUBSCRIPTION_CONNECTION_MINIMUM_IDLE_SIZE=1;
+    public static final Integer SUBSCRIPTION_CONNECTION_POOL_SIZE=50;
+    public static final Integer SLAVE_CONNECTION_MINIMUM_IDLE_SIZE=10;
+    public static final Integer SLAVE_CONNECTION_POOL_SIZE=64;
+    public static final Integer MASTER_CONNECTION_MINIMUM_IDLE_SIZE=10;
+    public static final Integer MASTER_CONNECTION_POOL_SIZE=64;
+    public static final String READ_MODE="SLAVE";
+    public static final String SUBSCRIPTION_MODE="SLAVE";
+    public static final Integer SCAN_INTERVAL=1000;
+}
---
+++ src/main/java/com/webank/weid/suite/persistence/redis/RedissonConfig.java
@@ -0,0 +1,60 @@
+package com.webank.weid.suite.persistence.redis;
+
+import com.webank.weid.constant.RedisDriverConstant;
+import com.webank.weid.util.PropertyUtils;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+
+/**
+ * @author karenli
+ * @program: weid-java-sdk
+ * @description:
+ * @date 2020-07-03 16:29:21
+ */
+
+public class RedissonConfig {
+
+    public RedissonConfig() {
+    }
+
+    //单节点模式
+    public RedissonClient redissonSingleClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress(PropertyUtils.getProperty(RedisDriverConstant.REDISSON_URL_SINGLE));
+        RedissonClient client= Redisson.create(config);
+
+        return client;
+    }
+
+    //集群模式
+    public RedissonClient redissonClusterClient()  {
+
+        Config config = new Config();
+
+         //集群状态扫描间隔时间，单位是毫秒, 可以用"rediss://"来启用SSL连接
+        try {
+            config.useClusterServers().setScanInterval(RedisDriverConstant.SCAN_INTERVAL)
+                    .addNodeAddress(PropertyUtils.getProperty(RedisDriverConstant.REDISSON_URL_CLUSTER).split(","))
+                    .setMasterConnectionMinimumIdleSize(RedisDriverConstant.MASTER_CONNECTION_MINIMUM_IDLE_SIZE)
+                    .setMasterConnectionPoolSize(RedisDriverConstant.MASTER_CONNECTION_POOL_SIZE)
+                    .setMasterConnectionMinimumIdleSize(RedisDriverConstant.MASTER_CONNECTION_MINIMUM_IDLE_SIZE)
+                    .setMasterConnectionPoolSize(RedisDriverConstant.MASTER_CONNECTION_POOL_SIZE)
+                    .setConnectTimeout(RedisDriverConstant.CONNECT_TIMEOUT)
+                    .setIdleConnectionTimeout(RedisDriverConstant.IDLE_CONNECTION_TIMEOUT)
+                    .setTimeout(RedisDriverConstant.TIMEOUT)
+                    .setRetryAttempts(RedisDriverConstant.RETRY_ATTEMPTS)
+                    .setRetryInterval(RedisDriverConstant.RETRY_INTERVAL);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        RedissonClient redisson = Redisson.create(config);
+
+        return redisson;
+    }
+
+}
+
