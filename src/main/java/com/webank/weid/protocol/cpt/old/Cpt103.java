978f3a3f25f150eb8a7ff059cf9665e66f32499c
CodingCattwo <847701726@qq.com>
2022-07-13 15:27:06 +0800

    rm fge

--- src/main/java/com/webank/weid/protocol/cpt/old/Cpt103.java
+++ src/main/java/com/webank/weid/protocol/cpt/old/Cpt103.java
@@ -19,10 +19,8 @@
 
 package com.webank.weid.protocol.cpt.old;
 
-import com.github.reinert.jjschema.Attributes;
-import lombok.Data;
-
 import com.webank.weid.protocol.base.Challenge;
+import lombok.Data;
 
 /**
  * Credential for authentication. The answer to meet the challenge. We package the answer into a
@@ -31,13 +29,13 @@ import com.webank.weid.protocol.base.Challenge;
  * @author Created by Junqi Zhang on 2019/4/9.
  */
 @Data
-@Attributes(title = "Authentication Answer", description = "Answer to meet the challenge")
+//@Attributes(title = "Authentication Answer", description = "Answer to meet the challenge")
 public class Cpt103 {
 
-    @Attributes(required = true, description = "The entity's weidentity did")
+//    @Attributes(required = true, description = "The entity's weidentity did")
     private String id;
-    @Attributes(required = true, description = "The challenge")
+//    @Attributes(required = true, description = "The challenge")
     private Challenge challenge;
-    @Attributes(required = true, description = "The proof")
+//    @Attributes(required = true, description = "The proof")
     private String proof;
 }
--- src/main/java/com/webank/weid/protocol/cpt/old/Cpt107.java
+++ src/main/java/com/webank/weid/protocol/cpt/old/Cpt107.java
@@ -19,12 +19,9 @@
 
 package com.webank.weid.protocol.cpt.old;
 
-import java.util.List;
-
-import com.github.reinert.jjschema.Attributes;
-import lombok.Data;
-
 import com.webank.weid.protocol.base.CredentialPojo;
+import java.util.List;
+import lombok.Data;
 
 /**
  * Multiple signature to a CredentialPojo.
@@ -33,9 +30,9 @@ import com.webank.weid.protocol.base.CredentialPojo;
  */
 
 @Data
-@Attributes(title = "Embedded Signature", description = "Embedded Signature object for multi-sign")
+//@Attributes(title = "Embedded Signature", description = "Embedded Signature object for multi-sign")
 public class Cpt107 {
 
-    @Attributes(required = true, description = "Original credential list to be signed")
+//    @Attributes(required = true, description = "Original credential list to be signed")
     List<CredentialPojo> credentialList;
 }
